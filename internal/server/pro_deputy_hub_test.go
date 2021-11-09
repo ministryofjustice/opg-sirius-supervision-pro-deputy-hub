967ee47e6d1a756fff01556c8b5551fa56eff290
Gurps Singh <60613823+gurps-singh@users.noreply.github.com>
2021-11-09 09:04:34 +0000

    SW-5238 add header and footer to skeleton #minor (#4)

---
+++ internal/server/pro_deputy_hub_test.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ministryofjustice/opg-sirius-supervision-pro-deputy-hub/internal/sirius"
+	"github.com/stretchr/testify/assert"
+)
+
+type mockProDeputyHubInformation struct {
+}
+
+func TestNavigateToDeputyHub(t *testing.T) {
+	assert := assert.New(t)
+
+	client := &mockProDeputyHubInformation{}
+	template := &mockTemplates{}
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("GET", "/path", nil)
+
+	handler := renderTemplateForProDeputyHub(client, template)
+	err := handler(sirius.PermissionSet{}, w, r)
+
+	assert.Nil(err)
+
+	resp := w.Result()
+	assert.Equal(http.StatusOK, resp.StatusCode)
+}
