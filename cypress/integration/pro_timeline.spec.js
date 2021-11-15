dbce5e237413e15e0e3db966a65e05f2037e1336
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2021-11-15 11:04:25 +0000

    sw-5287 fxed cypress test #minor

--- cypress/integration/pro_timeline.spec.js
+++ cypress/integration/pro_timeline.spec.js
@@ -12,7 +12,7 @@ describe("Timeline", () => {
     it("contains appropriate test data for a timeline event", () => {
         cy.get(".moj-timeline__title").should("contain", "New client added to deputyship");
         cy.get(".moj-timeline__byline").should("contain", "by system admin (12345678)");
-        cy.get("time").should("contain", "2021-09-09 14:01:59");
+        cy.get("time").should("contain", "09/09/2021 14:01:59");
         cy.get(".govuk-list > :nth-child(1)").should("contain", "Order number: 03305972");
         cy.get(".govuk-list > :nth-child(2)").should("contain", "Sirius ID: 7000-0000-1995");
         cy.get(".govuk-list > :nth-child(3)").should("contain", "Order type: pfa");
