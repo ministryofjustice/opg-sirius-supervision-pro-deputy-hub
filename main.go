9175e78e0b2aa7eb78b256dd34ff2ea622c90f82
samuel.joseph <denialanderror@googlemail.com>
2021-12-01 16:14:03 +0000

    Fixes broken error pages, due to missing Errors param. Renames server var to prevent dep name clash.

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"strings"
 	"syscall"
 	"time"
 
@@ -28,18 +27,6 @@ func main() {
 	layouts, _ := template.
 		New("").
 		Funcs(map[string]interface{}{
-			"join": func(sep string, items []string) string {
-				return strings.Join(items, sep)
-			},
-			"contains": func(xs []string, needle string) bool {
-				for _, x := range xs {
-					if x == needle {
-						return true
-					}
-				}
-
-				return false
-			},
 			"prefix": func(s string) string {
 				return prefix + s
 			},
@@ -61,13 +48,13 @@ func main() {
 		logger.Fatal(err)
 	}
 
-	server := &http.Server{
+	s := &http.Server{
 		Addr:    ":" + port,
 		Handler: server.New(logger, client, tmpls, prefix, siriusPublicURL, webDir),
 	}
 
 	go func() {
-		if err := server.ListenAndServe(); err != nil {
+		if err := s.ListenAndServe(); err != nil {
 			logger.Fatal(err)
 		}
 	}()
@@ -83,7 +70,7 @@ func main() {
 	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	if err := server.Shutdown(tc); err != nil {
+	if err := s.Shutdown(tc); err != nil {
 		logger.Print(err)
 	}
 }
