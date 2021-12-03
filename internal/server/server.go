bd140dfca7ceeefb0d223d738d3d9f0fab2f3654
samuel.joseph <denialanderror@googlemail.com>
2021-12-03 15:01:40 +0000

    SW-5280: Applied formatting

--- internal/server/server.go
+++ internal/server/server.go
@@ -106,7 +106,7 @@ type errorVars struct {
 	Path      string
 	Code      int
 	Error     string
-	Errors	  bool
+	Errors    bool
 }
 
 type ErrorHandlerClient interface {
@@ -158,7 +158,7 @@ func notFoundHandler(tmplError Template, siriusURL string) http.HandlerFunc {
 		_ = tmplError.ExecuteTemplate(w, "page", errorVars{
 			SiriusURL: siriusURL,
 			Code:      http.StatusNotFound,
-			Error: "Not Found",
+			Error:     "Not Found",
 		})
 	}
 }
